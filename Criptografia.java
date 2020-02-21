baf2b893432c5caaf886a0bbf706425187923a0c
matheusmaiagyn <58473563+matheusmaiagyn@users.noreply.github.com>
2020-02-21 18:18:43 -0300

    Organizando e reduzindo código

--- Criptografia.java
+++ Criptografia.java
@@ -1,27 +1,30 @@
 package criptografia;
 
-public class Criptografia {
+public class Criptografia 
+{
     
-    public Criptografia(){
+    public Criptografia()
+    {
         Criptografar();
         Descriptografar();
     }
-    
-    public void Criptografar(){
-        String msgto = "Teste msG";
-        int QtCrip = 4;
-        //System.out.println(Cript.Encriptar(msgto,QtCrip));
-        System.out.println(Cript.Encriptar(msgto,QtCrip, true));
-    }
-    
-    public void Descriptografar(){
-        String msgtode = "Rdsrd asC";
-        int QtCrip = 4;
-        //System.out.println(Decript.Decryp(msgtode, QtCrip));
-        System.out.println(Cript.Encriptar(msgto,QtCrip, false));
-    }
 
-    public static void main(String[] args) {
+    public void Criptografar()
+    {
+        String msgto = "Teste msG";
+        int QtCrip = 3;
+        System.out.println(Decript.Encriptar(msgto,QtCrip, true));
+    }
+    
+    public void Descriptografar()
+    {
+        String msgtode = "Vxlvx elH";
+        int QtCrip = 3;
+        System.out.println(Decript.Encriptar(msgtode,QtCrip, false));
+    }
+    
+    public static void main(String[] args) 
+    {
         new Criptografia();
     }
     
