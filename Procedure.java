0efdeabc67ab5b75024d9d0859b848ae783c7530
Matheus Maia <matheusmaiagyn@gmail.com>
2020-02-21 20:54:27 -0300

    Crtiptografia automatica teste

--- Procedure.java
+++ Procedure.java
@@ -5,24 +5,44 @@ import java.util.ArrayList;
 public class Procedure 
 {
 
-    public static String Encriptar(String msg, int QtCrip, boolean pEncrip)
+    public static String Encriptar(String msg, int QtCrip, boolean pEncrip, boolean pAutomatico)
     {
         String alphabet = "";
         String codalphabet = "";
+        String pFinalMsg = "";
+        String codmsg = "";
+        String msgtocrip = msg;
+        String pCodAuto = "mnbvcxzçlkjhgfdsapoiuytrewqMNBVCXZÇLKJHGFDSAPOIUYTREWQ";
+        int pQtdCrip = 1;
         
         if(pEncrip)
         {
             alphabet = "abcdefghijklmnopqrstuvxwyzç1234567890ABCDEFGHIJKLMNOPQRSTUVXWYZÇãõéôíá:., ";
             codalphabet = "qwertyuiopasdfghjklzxcvbnm#5467213908QWERTYUIOPASDFGHJKLZXCVBNM&õé[ãáí}*@ ";
+            
+            if(pAutomatico)
+            {
+                pQtdCrip = pCodAuto.charAt(QtCrip-1);
+                
+            }
         }else
         {
             alphabet = "qwertyuiopasdfghjklzxcvbnm#5467213908QWERTYUIOPASDFGHJKLZXCVBNM&õé[ãáí}*@ ";
             codalphabet = "abcdefghijklmnopqrstuvxwyzç1234567890ABCDEFGHIJKLMNOPQRSTUVXWYZÇãõéôíá:., ";
+            
+            if(msgtocrip.charAt(0)=='+')
+            {
+                for(int pDefineQtCrip=0; pDefineQtCrip < pCodAuto.length(); pDefineQtCrip++)
+                {
+                    if(msgtocrip.charAt(1)==pCodAuto.charAt(pDefineQtCrip))
+                    {
+                        pQtdCrip = pDefineQtCrip+1;
+                    }
+                }
+            }
         }
-
-        String codmsg = "";
-        String msgtocrip = msg;
-        for(int k=0; k < QtCrip; k++)
+        
+        for(int k=0; k < pQtdCrip; k++)
         { 
             for(int i=0; i < msgtocrip.length(); i++)
             {
@@ -37,6 +57,14 @@ public class Procedure
             msgtocrip = codmsg;
             codmsg = "";
         }
-        return msgtocrip.toString();
+        
+        if(pAutomatico)
+        {
+            pFinalMsg = "+" + pCodAuto.charAt(QtCrip);
+        }
+        
+        pFinalMsg += msgtocrip;
+
+        return pFinalMsg.toString();
     }
 }
