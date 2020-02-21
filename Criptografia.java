5f0c3797ac3f70131ee0736e2e1878b7b40a9a92
matheusmaiagyn <58473563+matheusmaiagyn@users.noreply.github.com>
2020-02-21 18:22:22 -0300

    Update Criptografia.java

--- Criptografia.java
+++ Criptografia.java
@@ -13,14 +13,14 @@ public class Criptografia
     {
         String msgto = "Teste msG";
         int QtCrip = 3;
-        System.out.println(Decript.Encriptar(msgto,QtCrip, true));
+        System.out.println(Procedure.Encriptar(msgto,QtCrip, true));
     }
     
     public void Descriptografar()
     {
         String msgtode = "Vxlvx elH";
         int QtCrip = 3;
-        System.out.println(Decript.Encriptar(msgtode,QtCrip, false));
+        System.out.println(Procedure.Encriptar(msgtode,QtCrip, false));
     }
     
     public static void main(String[] args) 
