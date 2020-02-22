7bcac43db063b76b40a8193f6cbad50c1937250a
Matheus Maia <matheusmaiagyn@gmail.com>
2020-02-22 01:40:07 -0300

    Melhorando testes durante desenvolvimento

--- Criptografia.java
+++ Criptografia.java
@@ -5,22 +5,19 @@ public class Criptografia
     
     public Criptografia()
     {
-        Criptografar();
-        Descriptografar();
+        Descriptografar(55, "Teste 40", false, false);
     }
 
-    public void Criptografar()
+    public String Criptografar(int QtCrip, String msgto, boolean pCripOrNot, boolean pAutoOrNot)
     {
-        String msgto = "Teste msG";
-        int QtCrip = 3;
-        System.out.println(Procedure.Encriptar(msgto,QtCrip, true));
+        String print = Procedure.Encriptar(msgto,QtCrip, true, true);
+        System.out.println(print);
+        return print;
     }
     
-    public void Descriptografar()
+    public void Descriptografar(int QtCrip, String msgto, boolean pCripOrNot, boolean pAutoOrNot)
     {
-        String msgtode = "Vxlvx elH";
-        int QtCrip = 3;
-        System.out.println(Procedure.Encriptar(msgtode,QtCrip, false));
+        System.out.println(Procedure.Encriptar(Criptografar(QtCrip, msgto, !pCripOrNot, pAutoOrNot), QtCrip, pCripOrNot, pAutoOrNot));
     }
     
     public static void main(String[] args) 
